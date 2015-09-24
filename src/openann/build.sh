8e5b99021eca481c2f4b2fd15afbff43b57d0e86
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2015-09-24 13:11:33 +0200

    OpenANN/eigen: use wget if hg fails during downloading lib eigen

--- src/openann/build.sh
+++ src/openann/build.sh
@@ -17,6 +17,13 @@ if [ ! -d eigen ]; then
   hg clone https://bitbucket.org/eigen/eigen/
 fi
 
+if [ ! -d eigen ]; then
+  wget http://bitbucket.org/eigen/eigen/get/3.2.5.tar.gz
+  tar xvfz 3.2.5.tar.gz
+  mv `ls -1 | grep eigen` eigen
+  rm -f 3.2.5.tar.gz
+fi
+
 if [ ! -d OpenANN ]; then
   ${GIT} clone https://github.com/OpenANN/OpenANN OpenANN
   (cd OpenANN/lib; sh ./setup.sh CMA-ESpp ALGLIB)
