854e1d1f0c5165fd231a8e5d2940c7dd1906dda9
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2017-12-08 13:30:50 +0100

    Fix cunieform-build

--- src/cuneiform/build.sh
+++ src/cuneiform/build.sh
@@ -11,28 +11,30 @@ fi
 
 if [ ! -d cuneiform ]; then
   ${GIT} clone https://github.com/joergen7/cuneiform.git cuneiform
-  (cd cuneiform; ${GIT} branch -d brn; git checkout -b brn f0297b542c87bba1100ac737560d96bd4a1afa70)
+  #(cd cuneiform; ${GIT} branch -d brn; git checkout -b brn f0297b542c87bba1100ac737560d96bd4a1afa70)
 fi
 
 if [ ! -d rebar3 ]; then
-  ${GIT} clone https://github.com/erlang/rebar3.git rebar3
-  (cd rebar3; ${GIT} branch -d brn; git checkout -b brn c21a5f7ab2309bedfd430e71ece576cbfaa93114)
+  #${GIT} clone https://github.com/erlang/rebar3.git rebar3
+  #(cd rebar3; ${GIT} branch -d brn; git checkout -b brn c21a5f7ab2309bedfd430e71ece576cbfaa93114)
+  mkdir rebar3
 fi
 
 echo "Build rebar3"
 
 if [ ! -f rebar3/rebar3 ]; then
-  (cd rebar3; ./bootstrap)
+  #(cd rebar3; ./bootstrap)
+  (cd rebar3; wget https://s3.amazonaws.com/rebar3/rebar3 && chmod +x rebar3)
 fi
 
 echo "Build cuneiform"
 
 if [ ! -f cuneiform/rebar3 ]; then
-  (cp cuneiform/rebar3 rebar3/)
+  cp rebar3/rebar3 cuneiform
 fi
 
-(cd cuneiform; PATH=$PATH:. make)
+(cd cuneiform; ./rebar3 escriptize)
 
-(cp cuneiform/_build/default/bin/cuneiform $BRN_TOOLS_PATH/click-brn-libs/bin/)
+cp cuneiform/_build/default/bin/cuneiform $BRN_TOOLS_PATH/click-brn-libs/bin/
 
 exit 0
