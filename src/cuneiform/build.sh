276c97f3c457b52238ce0b731465e92c495699b7
Robert Sombrutzki <sombrutz@informatik.hu-berlin.de>
2017-12-08 23:56:44 +0100

    Cuneiform: update build

--- src/cuneiform/build.sh
+++ src/cuneiform/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 GIT=/usr/bin/git
+REBAR3_GIT=0
 
 if [ "x$1" = "xclean" ]; then
   if [ -d cuneiform ]; then
@@ -9,26 +10,32 @@ if [ "x$1" = "xclean" ]; then
   exit 0
 fi
 
+if [ ! -d rebar3 ]; then
+  if [ "x$REBAR3_GIT" = "x1" ]; then
+    ${GIT} clone https://github.com/erlang/rebar3.git rebar3
+
+    #(cd rebar3; ${GIT} branch -d brn; git checkout -b brn c21a5f7ab2309bedfd430e71ece576cbfaa93114)
+  else
+    mkdir rebar3
+    (cd rebar3 && wget https://s3.amazonaws.com/rebar3/rebar3 && chmod +x rebar3)
+  fi
+
+  echo "Build rebar3"
+
+  if [ ! -f rebar3/rebar3 ]; then
+    (cd rebar3; ./bootstrap)
+  fi
+fi
+
 if [ ! -d cuneiform ]; then
   ${GIT} clone https://github.com/joergen7/cuneiform.git cuneiform
-  (cd cuneiform; ${GIT} branch -d brn; git checkout -b brn f0297b542c87bba1100ac737560d96bd4a1afa70)
-fi
-
-if [ ! -d rebar3 ]; then
-  ${GIT} clone https://github.com/erlang/rebar3.git rebar3
-  (cd rebar3; ${GIT} branch -d brn; git checkout -b brn c21a5f7ab2309bedfd430e71ece576cbfaa93114)
-fi
-
-echo "Build rebar3"
-
-if [ ! -f rebar3/rebar3 ]; then
-  (cd rebar3; ./bootstrap)
+  #(cd cuneiform; ${GIT} branch -d brn; git checkout -b brn f0297b542c87bba1100ac737560d96bd4a1afa70)
 fi
 
 echo "Build cuneiform"
 
 if [ ! -f cuneiform/rebar3 ]; then
-  (cp cuneiform/rebar3 rebar3/)
+  (cp rebar3/rebar3 cuneiform/)
 fi
 
 (cd cuneiform; PATH=$PATH:. make)
